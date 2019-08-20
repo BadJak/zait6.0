c41e4b151081e4c4bc70c4cde8181c291c5cf65e
Basil JelalEldean <soli.geu@gmail.com>
2019-08-21 00:32:04 +0100

    Zabet is on#2

--- app/overrides/update_home.rb
+++ app/overrides/update_home.rb
@@ -6,7 +6,7 @@ Deface::Override.new(:virtual_path => 'spree/shared/_nav_bar',
 <li id="search-bar" data-hook="">
       <form class="navbar-form" action="/products" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="✓">
   <div class="form-group">
-      <select name="taxon" id="taxon" aria-label="Taxon" class="form-control"><option value="">الأقسام</option>
+      <select name="taxon" id="taxon" aria-label="Taxon" class="form-control"><option value="">كل الأقسام</option>
 <option value="1">Categories</option>
 <option value="2">Brands</option></select>
   </div>
