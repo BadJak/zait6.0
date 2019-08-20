8a4804458210b716d0d37dbf4589e65ad5b93a78
Basil JelalEldean <soli.geu@gmail.com>
2019-08-21 00:25:16 +0100

    Zabet is on

---
+++ app/overrides/update_home.rb
@@ -0,0 +1,19 @@
+Deface::Override.new(:virtual_path => 'spree/shared/_nav_bar',
+  :name => 'change login language',
+  :replace => 'ul.nav',
+  :text => '<ul id="nav-bar" class="nav navbar-nav navbar-right" data-hook="">
+  <li id="link-to-login"><a href="/login">تسجيل دخول</a></li>
+<li id="search-bar" data-hook="">
+      <form class="navbar-form" action="/products" accept-charset="UTF-8" method="get"><input name="utf8" type="hidden" value="✓">
+  <div class="form-group">
+      <select name="taxon" id="taxon" aria-label="Taxon" class="form-control"><option value="">الأقسام</option>
+<option value="1">Categories</option>
+<option value="2">Brands</option></select>
+  </div>
+  <div class="form-group">
+    <input type="search" name="keywords" id="keywords" placeholder="ما الذي تبحث عنه؟" class="form-control">
+  </div>
+  <input type="submit" value="بحث" class="btn btn-success" data-disable-with="Search">
+</form>
+    </li>
+  </ul>')
